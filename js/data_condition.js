e4e10c3937607a59c1be8f2fa7b367ec929dceeb
CaveDann <74264942+CaveDann@users.noreply.github.com>
2021-04-07 16:39:59 +0100

    Expand conditions

--- js/data_condition.js
+++ js/data_condition.js
@@ -1,5 +1,15 @@
 data_condition = [
     {
+        title: "Blessed",
+        icon: "beams-aura",
+        subtitle: "You are blessed",
+        description: "You are blessed",
+        reference: "PHB, pg. 219.",
+        bullets: [
+            "You can roll 1d4 whenever you make an attack roll or saving throw and add it to the total for the roll."
+        ]
+    },
+    {
         title: "Blinded",
         icon: "one-eyed",
         subtitle: "You can't see",
@@ -20,6 +30,35 @@ data_condition = [
         bullets: [
             "You can't attack your charmer or target them with harmful abilities or magical effects.",
             "Your charmer has advantage on ability checks to interact socially with you."
+        ]
+    },
+    {
+        title: "Concentrating",
+        icon: "brain",
+        subtitle: "You are concentrating",
+        description: "You are concentrating",
+        reference: "PHB, pgs. 203, 204.",
+        bullets: [
+            "You can end concentration at any time (no action required).",
+            "You lose concentration on a spell if you cast another spell that requires concentration. You cannot concentrate on two spells at once.",
+	    "Whenever you take damage, you must make a Constitution saving throw to maintain concentration. The DC equals 10 or half the damage taken, whichever is higher. If you are damaged by multiple sources, you repeat the save for each source.",
+	    "You lose concentration if you are incapacitated or die.",
+	    "Environmental phenomena, such as a sudden flash of lightning or a wave crashing over you, may require you to make a DC 10 Constitution saving throw at the DM's discretion.",
+            "If you lose concentration, the spell ends."
+        ]
+    },
+    {
+        title: "Cursed",
+        icon: "imp-laugh",
+        subtitle: "You are cursed",
+        description: "You are cursed",
+        reference: "PHB, pg. 218.",
+        bullets: [
+            "Curses are often limited to one of the effects listed below.",
+	    "You have disadvantage on ability checks and saving throws made with the affected ability score.",
+            "You have disadvantage on attack rolls against the bestower of your curse.",
+            "You must make a Wisdom saving throw at the start of each of your turns, else lose your action.",
+	    "You take additional damage from the bestower of your curse."
         ]
     },
     {
@@ -52,6 +91,18 @@ data_condition = [
         ]
     },
     {
+        title: "Enlarged",
+        icon: "inner-self",
+        subtitle: "You are enlarged",
+        description: "You are enlarged",
+        reference: "PHB, pg. 237.",
+        bullets: [
+            "Your size doubles in all dimensions and your weight multiplies by eight. Your size category increases by one.",
+            "You have advantage on Strength checks and Strength saving throws.",
+ 	    "If your weapons are enlarged with you, you deal an additional 1d4 damage with them."
+        ]
+    },
+    {
         title: "Exhaustion",
         icon: "crawl",
         subtitle: "You are exhausted",
@@ -62,6 +113,19 @@ data_condition = [
             "You suffer the effect of your current level of exhaustion as well as all lower levels.",
             "Finishing a long rest reduces your exhaustion level by 1, provided that you have also had some food and drink.",
             "Also, being raised from the dead reduces a creature’s exhaustion level by 1."
+        ]
+    },
+    {
+        title: "Falling",
+        icon: "falling",
+        subtitle: "Modifier: 500ft per turn",
+        reference: "PHD, pg. 183 | XGE, pg. 77.",
+        description: "Descend up to 500ft per turn",
+        bullets: [
+            "When you start to fall, you immediately descend up to 500 feet.",
+            "If you're still falling on your next turn, you descend up to 500 feet at the end of that turn. You continue falling until you hit the ground or the fall is halted.",
+	    "At the end of a fall, you take 1d6 bludgeoning damage for every 10 feet you fell, up to a maximum of 20d6. You land prone unless you avoid taking damage from the fall.",
+            "If you have a non-zero flying speed, and are falling at the start of your turn, you can spend half your flying speed to counter the fall (as if standing up from the prone condition)."
         ]
     },
     {
@@ -86,6 +150,51 @@ data_condition = [
             "The condition ends if your grappler is incapacitated.",
             "The condition also ends if you are removed from the reach of your grappler."
         ]
+    },
+    {
+        title: "Flying",
+        icon: "feathered-wing",
+        subtitle: "You are flying",
+        reference: "PHB, pg. 191.",
+        description: "You are flying",
+        bullets: [
+            "If you are knocked prone, have your speed reduced to zero, or are otherwise unable to move, you begin to fall, unless you have the ability to hover or are held aloft by magic such as the fly spell."
+        ]
+    },
+    {
+        title: "Hastened",
+        icon: "wingfoot",
+        subtitle: "You are hastened",
+        description: "You are hastened",
+        reference: "PHB, pg. 250.",
+        bullets: [
+            "Your speed is doubled.",
+	    "You have a +2 bonus to your AC.",
+	    "You have advantage on Dexterity saving throws.",
+	    "You can take an additional action on each of your turns. The action can only be used to take the Attack (one weapon attack only), Dash, Disengage, Hide, or Use an Object action.",
+	    "When the hastened condition ends, you cannot move or take actions until after your next turn."
+        ]
+    },
+    {
+        title: "Hexed",
+        icon: "barbed-wire",
+        subtitle: "You are hexed",
+        description: "You are hexed",
+        reference: "PHB, pg. 251.",
+        bullets: [
+            "You take oadditional damage from the source of your hex.",
+            "You have disadvantage on ability checks made with the affected ability."
+        ]
+    },
+    {
+        title: "Hunter's Mark",
+        icon: "high-shot",
+        subtitle: "You are marked",
+        description: "You are marked",
+        reference: "PHB, pg. 251.",
+        bullets: [
+            "You take additional damage from the source of your Hunter's Mark."
+	]
     },
     {
         title: "Incapacitated",
@@ -157,6 +266,21 @@ data_condition = [
             "You have disadvantage on attack rolls.",
             "Attack rolls against you have advantage if the attacker is within 5 feet of you, otherwise the attack roll has disadvantage.", 
 	    "If you are flying, you start to fall and immediately descend up to 500 feet."
+        ]
+    },
+    {
+        title: "Raging",
+        icon: "muscle-up",
+        subtitle: "You are raging",
+        description: "You are raging",
+        reference: "PHB, pg. 48.",
+        bullets: [
+            "While raging, you gain the following benefits if you aren't wearing heavy armour.",
+            "You have advantage on Strength checks and Strength saving throws.",
+            "When you make a melee weapon attack using Strength, you gain a bonus to the damage roll determined by your level.",
+	    "You have resistance to bludgeoning, piercing and slashing damage.",
+	    "You cannot cast or concentrate on spells whilst raging.",
+	    "You may gain additional benefits from your subclass whilst raging."
         ]
     },
     {
