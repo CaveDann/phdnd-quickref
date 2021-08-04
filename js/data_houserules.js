7e2d75c4d3c8a72d16636ce96d00189c0eecb426
CaveDann <74264942+CaveDann@users.noreply.github.com>
2021-08-04 14:48:58 +0100

    Add House Rules section & populate

---
+++ js/data_houserules.js
@@ -0,0 +1,127 @@
+data_houserules = [
+    {
+        title: "Birthday Boons",
+        icon: "beer-stein",
+        subtitle: "Not in use",
+        description: "Things for player birthdays",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "On the game closest to your birthday, we might figure out some in-game reward for you. Could be a supernatural boon, could be a magic item.",
+            "This has not been done since everyone got their first Birthday item in ToA."
+        ]
+    },
+    {
+        title: "Breath Weapon",
+        icon: "dragon-head",
+        subtitle: "Dragonborn feat",
+        description: "Improves at 8th-level",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "From 8th-level, Dragonborn may use their breath weapon as either an action or a bonus action."
+        ]
+    },
+    {
+        title: "Combat",
+        icon: "crossed-swords",
+        subtitle: "2m turn timer",
+        description: "Turns should be fast and on-initiative",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "In 5E, you cannot defer your turn. Ideally, your turn shouldn't take longer than two minutes. Depending on the pace of the game, the DM may crack the whip.",
+            "If time runs out before you finish your turn, the game may move on."
+        ]
+    },
+    {
+        title: "Critical Hits",
+        icon: "crystal-shine",
+        subtitle: "Max + additional roll",
+        description: "When you roll a natural 20 with an attack roll",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "When a critical hit renders a PC unconscious, the player can choose if the hit leaves a scar or not. The location can be chosen by the player, or determined by a roll of the body die (thanks Simon).",
+            "When a PC scores a critical hit, maximise one set of the die instead of rolling double. I.e. An attack that deals 1d6 + 2 damage, deals max(1d6) + 1d6 + 2 damage on a crit."
+        ]
+    },
+    {
+        title: "Errata",
+        icon: "bookmarklet",
+        subtitle: "When RAW ≠ RAI",
+        description: "Use errata",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "Use errata."
+        ]
+    },
+    {
+        title: "Feats",
+        icon: "embrassed-energy",
+        subtitle: "Enabled",
+        description: "Choose feats instead of ASI increase",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "Feats are enabled."
+        ]
+    },
+    {
+        title: "Multiple Ability Checks",
+        icon: "drama-masks",
+        subtitle: "Only if meaningful",
+        description: "Only if meaningful, repeat check",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "Sometimes a character fails an ability check and wants to try again. Generally, a second attempt won't be allowed unless the circumstances of the check can be changed in a meaningful way. Perhaps Abby is trying to pry open a door, but initially fails. If Brad was to apply pressure to the door using a crowbar, Abby could attempt to pry the door open again, as the circumstances have changed in a meaningful way.",
+            "In some cases, a character may be free to attempt a check multiple times; the only real cost is the time it takes. In other cases, failing an ability check makes it impossible to make the same check to do the same thing again. For example, a lie told twice won't work.",
+            "Any repeated check must abide by the spirit of the game. Repeating a stealth check to sneak up on a location won't be allowed just because the result was low."
+        ]
+    },
+    {
+        title: "Potions of Healing",
+        icon: "potion-ball",
+        subtitle: "Maximise healing",
+        description: "Maximise healing",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "When using any level of potion of healing, take the maximum possible result as the hit points restored. I.e. 2d4 + 2 would restore 10 hit points."
+        ]
+    },
+    {
+        title: "Racial ASI",
+        icon: "muscle-up",
+        subtitle: "Be who you want",
+        description: "Unbound, Unbent, Unbroken",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "When you select a race with ability score increases (ASI), you can apply these bonuses to any ability rather than those described. You cannot apply two sets of bonuses to the same ability."
+        ]
+    },
+    {
+        title: "Revised Ranger",
+        icon: "double-shot",
+        subtitle: "Enabled",
+        description: "Danny's Revised Ranger",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "You can use the Revised Ranger build instead of the Ranger build from the PHB if you want."
+        ]
+    },
+    {
+        title: "Starting Gear",
+        icon: "battle-gear",
+        subtitle: "Don't roll gold",
+        description: "Take what is offered",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "Just take the starting equipment from your background and class. Don't roll for gold."
+        ]
+    },
+    {
+        title: "Variant Human",
+        icon: "duality",
+        subtitle: "Enabled",
+        description: "You but with feats",
+        reference: "Last Updated: 04/08/2021",
+        bullets: [
+            "I think I'm ok with these being used whenever. If this changes, I'll update you all."
+        ]
+    }
+]
